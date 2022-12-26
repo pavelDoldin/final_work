29119752e8ef8abdc4742c51704f55470e684e27
pavelDoldin <doldin86@icloud.com>
2022-12-26 22:30:48 +0500

    code

---
+++ code/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая из имеющегося массива строк формирует массив из строк, 
+// длинна которых меньше или равна 3 симвала. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+
+void FillArraySecond(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+    if(arrayFirst[i].Length <= 3)
+        {
+        arraySecond[count] = arrayFirst[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] arrayFirst = new string[4] {"hello", "2", "world", ":-)"};
+string[] arraySecond = new string[arrayFirst.Length];
+FillArraySecond(arrayFirst, arraySecond);
+PrintArray(arraySecond);
